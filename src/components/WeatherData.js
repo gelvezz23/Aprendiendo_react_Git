9f37b67095b04292bb52726a92d1259e17f6c48f
gelvezz23 <mao_23_@hotmail.com>
2019-01-21 23:42:18 -0500

    Imports para iconos usando componentes reactivos

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import WeatherTemperatura from './WeatherTemperatura';
 import WeatherExtraInfo from './WeatherExtraInfo';
-
-
+import {
+    CLOUD,
+CLOUDY,
+SUN, 
+RAIN, 
+SNOW, 
+WINDY,
+} from './weathers';
 
 const WeatherData = () => (
     <div>
 <WeatherTemperatura 
     temperature = {20} 
-    weatherState = {'cloud'}
+    weatherState = {CLOUDY}
     />
 <WeatherExtraInfo humidity = {80} wind={"10 m/s "} />
     </div>
