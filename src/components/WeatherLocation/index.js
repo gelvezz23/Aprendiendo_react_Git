232948412afe3addf319744ed93db5f10f1f0622
gelvezz23 <mao_23_@hotmail.com>
2019-04-04 18:26:07 -0500

    arreglando errores de import Style.css

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -2,11 +2,27 @@ import React from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import PropTypes from 'prop-types';
+import {
+    SUN,
+
+} from './../../../constants/weathers';
+
+
+
+const data = {
+
+    temperature: 5,
+    weatherState: SUN,
+    humidity: 10,
+    wind:'10 m/s',
+
+
+}
 
 const WeatherLocation = () => (
     <div>
 <Location city = {'Colombia'}></Location>
- <WeatherData></WeatherData>
+ <WeatherData data= {data}></WeatherData>
  </div>
 );
 // en la pagina de facebook esta mas sobre proptypes
